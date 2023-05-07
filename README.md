f745b3b1f00169288b8ffddb82085632ff47bc66
marnvel <ricardom.velarde@gmail.com>
2023-05-07 20:44:11 -0300

    Se dejó en funcionamiento el Punto 1. Restaría mejorar un poco la IA, para informar al usuario lo que se hace por detrás, es decir cuál es la función que utiliza y cual es el 'x' de entrada al modelo que produce la predicción que se muestra al presionar el botón 'Predecir'. 07-05-2023 20:44hs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Este repositorio contiene dos ejemplos que responden a las siguientes consignas:
 En este ejemplo, se presenta un sitio web que utiliza TensorFlow.js para entrenar un modelo de regresión lineal. El usuario puede ingresar la cantidad de veces que desea que el modelo se entrene antes de predecir los valores. 
 
 ## Punto 2: que solicite al usuario el valor de x para predecir el valor de y
-En este ejemplo, se presenta un sitio web que utiliza TensorFlow.js para predecir valores de `y` a partir de un valor de `x` ingresado por el usuario. El modelo de regresión lineal ya ha sido entrenado previamente y está alojado en el sitio.
+En este ejemplo, se presenta un sitio web que utiliza TensorFlow.js para predecir valores de `y` a partir de un valor de `x` ingresado por el usuario. El modelo de regresión lineal ya ha sido entrenado previamente.
 
 Ambos ejemplos siguen las bases del tutorial de TensorFlow.js disponible en el siguiente enlace:
 https://www.tensorflow.org/js/tutorials?hl=es-419
--- clase3_05_Modelo_secuencial.js
+++ clase3_05_Modelo_secuencial.js
@@ -1,7 +1,91 @@
 
 const model = tf.sequential();
 
-async function run()  {
+// Obtengo los elementos HTML
+let inputNumEpocas = document.getElementById('iInputEpocas');
+const btnEntrenar = document.getElementById('btn_entrenar');
+const btnPredecir = document.getElementById('btn_predecir');
+const outDiv = document.getElementById('micro-out-div');
+
+// Variable para almacenar el número ingresado por el usuario
+let num;
+let prevNum;
+
+
+// Evento de input para validar el número ingresado y habilitar el botón "Entrenar"
+
+inputNumEpocas.addEventListener( 'input', (event) => {
+    
+    // console.log("Imprimo el input", event.target.value);
+    // console.log("Imprimo el TIPO del input", typeof(event.target.value));
+
+    // Obtener el valor del input
+    num = parseInt(inputNumEpocas.value);
+
+    // console.log("Imprimo el num", num);
+    // console.log("Imprimo el TIPO del num", typeof(num));
+
+    // Validar que sea un número entero
+    if (!isNaN(num) && Number.isInteger(num) && num > 0) {
+        // Habilitar el botón "Entrenar"
+        outDiv.innerHTML = ``;
+        btnEntrenar.disabled = false;
+    } else {
+        // Deshabilitar el botón "Entrenar"
+        btnEntrenar.disabled = true;
+        console.warn("Debe ingresar n número entero POSITIVO!");
+        outDiv.innerHTML = `
+            <div class="alert alert-warning" role="alert">
+                Debe ingresar un número entero POSITIVO!
+            </div>`;
+    }
+});
+
+
+btnEntrenar.addEventListener('click', function() {
+    // Deshabilitar el input
+    inputNumEpocas.disabled = true;
+    // Mostrar alert rojo
+    outDiv.innerHTML = `
+        <div class="alert alert-danger" role="alert">
+            Entrenando modelo. Espere por favor... 💬 🕐...
+        </div>`;
+    // Deshabilitar el botón "Entrenar"
+    btnEntrenar.disabled = true;
+    // Llamar a la función de entrenamiento
+    run(num);
+});
+
+btnPredecir.addEventListener('click', function () {
+
+    //Realizar la predicción:
+    const resultado = Predecir();
+
+    // Limpiar el input
+    inputNumEpocas.value = "";
+
+    //Habilitar el input
+    inputNumEpocas.disabled = false;
+
+    // Deshabilitar el botón "Predecir"
+    btnPredecir.disabled = true;
+
+    // Mostrar alert azul con el resultado de la predicción
+    outDiv.innerHTML = `
+        <div class="alert alert-primary" role="alert">
+            <p> El resultado de la predicción es: 
+                <strong>${resultado}</strong>
+            </p>
+        </div>`;
+
+});
+
+
+
+
+
+
+async function run(num)  {
     /*
     Creamos con tf un modelo que predice cuando el usuario ingrese un valor x, la computadora
 
@@ -17,9 +101,19 @@ async function run()  {
     const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
     
     // Train the model using the data.
-    await model.fit(xs, ys, {epochs: 500});
+    await model.fit(xs, ys, {epochs: num});
 
-    document.getElementById('micro-out-div').innerText = "Terminé de entrenar";
+    
+    // Mostrar alert verde
+    outDiv.innerHTML = `
+        <div class="alert alert-success" role="alert">
+            Terminé de entrenar el modelo 🤖
+        </div>`;
+
+    // document.getElementById('micro-out-div').innerText = "Terminé de entrenar";
+
+    // Habilitar el botón "Predecir"
+    btnPredecir.disabled = false;
 }
 // run();
 
@@ -27,6 +121,8 @@ function Predecir () {
     // Use the model to do inference on a data point the model hasn't seen.
     // Should print approximately 39.
     
-    document.getElementById('micro-out-div').innerText =
-    model.predict(tf.tensor2d([20], [1, 1])).dataSync();
- }
+    // document.getElementById('micro-out-div').innerText = model.predict(tf.tensor2d([20], [1, 1])).dataSync();
+    const resultado = model.predict(tf.tensor2d([20], [1, 1])).dataSync();
+    return resultado
+}
+
